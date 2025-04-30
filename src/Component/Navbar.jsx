171e7fcac61b7c8d86318faf193f8870d43c3a93
Jishan <kazijishan50@gmail.com>
2025-04-30 22:32:31 +0530

    Navbar Completed

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,34 +1,39 @@
 import React from "react";
 export const Navbar = () => {
   return (
-    <div className="bg-[var(--bg-color)] h-17 w-lvw ">
-      <div className="flex justify-between items-center h-full px-3">
+    <>
+    <div className="h-[80px] w-full bg-[var(--bg-color)] shadow-md content-center fixed top-0 z-10"> 
+
+    <div className="">
+
+      <div className="w-full flex justify-between items-center h-full pr-5 max-w-[1400px] mx-auto">
         <img
-          className="h-auto w-[30%]"
+          className="h-[100%] w-[auto] max-w-[120px] object-contain  rounded-lg ml-2"
           src="./src/assets/logo.png"
           alt="Logo"
         />
+
+        <input type="text" className="hidden md:inline h-10 w-[40%] border border-gray-300 rounded-3xl pl-5 text-[var(--text-color)]" placeholder="Search..." />
+
         <div className="flex items-center">
           {" "}
           {/* Added a div for alignment */}
-          <ul className="flex gap-5 hidden">
+          <ul className="flex gap-5 hidden md:flex">
             <li className="text-[var(--text-color)]">Home</li>
             <li className="text-[var(--text-color)]">About</li>
             <li className="text-[var(--text-color)]">Contact</li>
           </ul>
-          
-          <div className="flex gap-5 justify-between items-center">
-            
-            <button className="h-5 w-[20px] bg-[url(./src/assets/search.svg)] bg-no-repeat bg-cover cursor-pointer">
-            </button>
-            <button className="h-5 w-[20px] bg-[url(./src/assets/bars-solid.svg)] bg-no-repeat bg-cover cursor-pointer">
-            </button>
-          </div>
-            
 
+          <div className="h-[50px] w-[15vw] flex gap-5 justify-between items-center md:hidden" >
+            <button className="h-[20px] w-[20px] bg-[url(./src/assets/search.svg)] bg-no-repeat bg-cover cursor-pointer"></button>
+
+            <button className="h-[20px] w-[20px] bg-[url(./src/assets/bars-solid.svg)] bg-no-repeat bg-cover cursor-pointer"></button>
+          </div>
         </div>{" "}
         {/* Closing the added div */}
       </div>
     </div>
+    </div>
+    </>
   );
 };
