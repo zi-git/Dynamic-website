02548bdf1e91b822baae613c7b2217dbc1844ea7
Jishan <kazijishan50@gmail.com>
2025-05-03 00:29:12 +0530

    Animated uptill JS div

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Hero() {
   return (
     <>
-      <div className="h-[auto] w-[100vw] bg-linear-[0deg,black,#00093f,#000939_80%,black] pt-50 font-['Segoe UI', 'Roboto', 'Helvetica Neue', sans-serif]">
+      <div className="h-[auto] w-[100vw] bg-linear-[0deg,black,#00093f,#000939_80%,black] pt-50 font-['Segoe UI', 'Roboto', 'Helvetica Neue', sans-serif] ">
         <div className="h-[30vh] w-[100vw] grid grid-cols-1 grid-rows-[3.80rem_5rem_6vh] gap-5 justify-items-center  text-center font-['Segoe UI', 'Roboto', 'Helvetica Neue', sans-serif]">
           <h4 className="h-15 text-center text-[transparent] bg-linear-to-r from-gray-600 to-gray-300 bg-clip-text text-5xl font-bold font-['Segoe UI', 'Roboto', 'Helvetica Neue', sans-serif]">
             Hey There
@@ -27,22 +27,22 @@ export default function Hero() {
         </div>
 
         <div className="h-[auto] flex justify-center items-center gap-10 flex-col mt-10 pb-100">
-          <h4 className="h-15 w-[90vw] text-center text-[transparent] bg-linear-to-r from-gray-600 to-gray-300 bg-clip-text text-3xl font-bold font-['Segoe UI', 'Roboto', 'Helvetica Neue', sans-serif]">
+          <h4 className="h-15 w-[90vw] text-center text-[transparent] bg-linear-to-r from-gray-700 to-gray-300 bg-clip-text text-3xl font-bold font-['Segoe UI', 'Roboto', 'Helvetica Neue', sans-serif] animate-[var(--showly-visible)]">
             {" "}
             This website is made with
           </h4>
 
           {/* HTML */}
 
-          <div className="h-[auto] flex justify-center items-center  gap-10 flex-col sm:w-[90vw] sm:max-w-[1200px] sm:grid sm:grid-cols-3 sm:grid-rows-3">
+          <div className="h-[auto] flex justify-center items-center  gap-10 flex-col sm:w-[90vw] sm:max-w-[1200px] sm:grid sm:grid-cols-3 sm:grid-rows-3 ">
             <div className=" h-[50vh] w-[80vw] max-w-[350px] bg-[#00000079] rounded-2xl shadow-[0px_0px_10px_indigo] p-[20px] flex flex-col justify-around items-center sm:h-[50vh] sm:w-[auto] sm:max-w-[350px] sm:min-w-[270px]">
               <img
-                className=" rotate-x-30 rotate-z-25 h-auto w-[68%] rounded-4xl border-2 border-gray-500 p-5 shadow-[14px_14px_15px_#E34F26ff] "
+                className=" rotate-x-30 rotate-z-25 h-auto w-[68%] rounded-4xl border-2 border-gray-500 p-5 shadow-[14px_14px_15px_#E34F26ff] animate-[var(--dimming_effect)] "
                 src="/src/assets/html.png"
                 alt="HTML"
               />
-              <h5 className=" text-3xl text-[#E34F26ff] self-start ">HTML</h5>
-              <p className=" text-[var(--text-color)]">
+              <h5 className="text_scale_effect text-3xl text-[#E34F26ff] self-start ">HTML</h5>
+              <p className=" text_scale_effect text-[var(--text-color)]">
                 HTML (HyperText Markup Language) is the standard language used
                 to create and structure content on the web.
               </p>
@@ -50,9 +50,9 @@ export default function Hero() {
 
             {/* css */}
 
-            <div className="h-[50vh] w-[80vw] max-w-[350px] bg-[#00000079] rounded-2xl shadow-[0px_0px_10px_indigo] p-[20px] flex flex-col justify-around items-center sm:h-[50vh] sm:w-[auto] sm:col-start-2 sm:col-end-4 sm:max-w-[770px] sm:min-w-[270px] ">
+            <div className="left_to_right  h-[50vh] w-[80vw] max-w-[350px] bg-[#00000079] rounded-2xl shadow-[0px_0px_10px_indigo] p-[20px] flex flex-col justify-around items-center sm:h-[50vh] sm:w-[auto] sm:col-start-2 sm:col-end-4 sm:max-w-[770px] sm:min-w-[270px] ">
               <img
-                className=" rotate-x-30 rotate-z-25 h-[52%] w-[69%] rounded-4xl border-2 border-gray-500 p-3 shadow-[14px_14px_14px_#039BE5ff] "
+                className="right_to_left_image  rotate-x-30 rotate-z-25 h-[52%] w-[69%] rounded-4xl border-2 border-gray-500 p-3 shadow-[14px_14px_14px_#039BE5ff] "
                 src="/src/assets/css.png"
                 alt="CSS"
               />
@@ -66,7 +66,7 @@ export default function Hero() {
 
             {/* JavaSrc */}
 
-            <div className="h-[50vh] w-[80vw] max-w-[350px] bg-[#00000079] rounded-2xl shadow-[0px_0px_10px_indigo] p-[20px] flex flex-col justify-around items-center sm:h-[50vh] sm:w-[auto]  sm:max-w-[770px] sm:min-w-[270px] sm:col-start-1 sm:row-start-2 sm:col-end-3 ">
+            <div className="bottom_to_top h-[50vh] w-[80vw] max-w-[350px] bg-[#00000079] rounded-2xl shadow-[0px_0px_10px_indigo] p-[20px] flex flex-col justify-around items-center sm:h-[50vh] sm:w-[auto]  sm:max-w-[770px] sm:min-w-[270px] sm:col-start-1 sm:row-start-2 sm:col-end-3 ">
               <img
                 className=" rotate-x-30 rotate-z-25 h-auto w-[70%] rounded-4xl border-2 border-gray-500 p-3 shadow-[14px_14px_14px_#F4E11Eff] "
                 src="/src/assets/js.png"
